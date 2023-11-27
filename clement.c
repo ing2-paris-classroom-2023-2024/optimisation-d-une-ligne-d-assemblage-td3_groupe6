8f1bfcd9603ec5a7a168322d19e6f5c6fc2fd7cd
AzogSmash <clement.casse@edu.ece.fr>
2023-11-27 15:02:48 +0100

    Initialisation branc Clem

---
+++ clement.c
@@ -0,0 +1,5 @@
+//
+// Created by ccass on 27/11/2023.
+//
+
+#include "clement.h"
---
+++ main.c
@@ -0,0 +1,13 @@
+//
+// Created by ccass on 27/11/2023.
+//
+
+#include <stdio.h>
+
+
+int main() {
+    int a = 0;
+    printf("%d", a );
+    return 0 ;
+
+};
